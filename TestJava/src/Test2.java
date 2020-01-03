de48b74340bb2c3ff62f763d03ebf0538d0379e9
Andre <s4nd123@gmail.com>
2020-01-04 01:01:37 +0800

    Third Commit
    Signed-off-by: Andre <s4nd123@gmail.com>

--- TestJava/src/Test2.java
+++ TestJava/src/Test2.java
@@ -1,8 +1,9 @@
 
-public class Test {
+public class Test2 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Hello World Andre");
+		System.out.println("Hello Andre");
 	}
+
 }
